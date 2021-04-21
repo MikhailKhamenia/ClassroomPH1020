e276f8a0f25102e18aba7cff28b23fce60a21b87
MikhailKhamenia <82834963+MikhailKhamenia@users.noreply.github.com>
2021-04-21 20:25:17 +0300

    Update russian_input_class.py

--- russian_input_class.py
+++ russian_input_class.py
@@ -1,21 +1,16 @@
-Python 3.9.2 (tags/v3.9.2:1a79785, Feb 19 2021, 13:44:55) [MSC v.1928 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> class Ввод():
-	def __call__(self, *args, **kwargs):
-		reuturn input
-		
-SyntaxError: invalid syntax
->>> class Ввод():
+class Ввод():
 	def __call__(self, *args, **kwargs):
 		return input(*args, **kwargs)
 
-	
->>> ввод_имени=Ввод()
->>> ввод_возраста=Ввод()
->>> имя=ввод_имени("Введите ваше имя")
-Введите ваше имяМихаил Хаменя
->>> возраст=ввод_возраста("Сколько лет")
-Сколько лет18
->>> возраст
-'18'
->>> 
+def печать(*args, **kwargs):
+    return print(*args, **kwargs)
+	    
+
+def main():
+    ввод_имени=Ввод()
+    ввод_возраста=Ввод()
+    имя=ввод_имени("ВВод имени")
+    лет=ввод_возраста("Сколько лет")
+    печать(f"Привет {имя}, вам {лет} лет")
+
+if __name__=="__main__":main()
